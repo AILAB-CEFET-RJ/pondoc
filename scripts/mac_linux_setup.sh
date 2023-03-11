a9384768073ab1639ef4a43a4614175dbe983a83
Gabriel Camargo <grocre@gmail.com>
2023-03-11 18:49:13 -0300

    Arquivo script do mac/linux modificado

--- scripts/mac_linux_setup.sh
+++ scripts/mac_linux_setup.sh
@@ -1,4 +1,4 @@
 sudo python3 -m venv venv # cria a virtualenv
 source venv/bin/activate # ativa a venv
 
-pip install requirements.txt # instala as dependências que estão no arquivo requirements.txt
+pip install -r requirements.txt # instala as dependências que estão no arquivo requirements.txt
